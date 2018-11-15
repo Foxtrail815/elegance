496d4ea8966373b810f5bd6334d785e7228da86f
Nick <lennin20@hotmail.com>
2018-11-14 22:27:21 -0600

    Added dinos widget

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
 
+  resources :dianosaurs
   devise_for :users
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/widgets/:template', to: 'widgets#show'
+  
 end
